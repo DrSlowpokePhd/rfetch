60bad595cee1635a36aba11e11129b18d18eedd6
DrSlowpokePhd <dohertyjames1999@gmail.com>
2019-08-06 22:30:24 -0700

    Added margin functionality to the boxed function as well as helper functions for boxed

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,54 @@
 use std::time::SystemTime;
 use std::env;
 
-fn boxed(input: &str) {
+// helper functions for the boxed function
+fn print_times(content: &str, times: u8) {
+    // prints a string a set amount of times
+    for _ in 0..times {
+        print!("{}", content)
+    }
+}
+
+fn print_block(width: u8, times: u8) {
+    // prints a vertical block with a width of
+    // spaces between bars
+    let mut block = String::new();
+    block.push_str("┃");
+    for _ in 0..width {
+        block.push_str(" ");
+    }
+    block.push_str("┃");
+    println!("{}", block);
+}
+
+fn boxed(input: &str, margin: u8) {
+    // added a margin parameter to allow for
+    // adjustment of margin of the box
     // ┃┏ ┓┛━┓ ┗┃┛┗ ┏━
     print!("┏");
+
+    print_times("━", margin);
+
     for x in input.chars() {
         print!("━");
     }
+    print_times("━", margin);
     println!("┓");
+    print_block(2*margin + input.len() as u8, margin);
     print!("┃");
+    print_times(" ", margin);
     print!("{}", input);
+    print_times(" ", margin);
     println!("┃");
+    print_block(2*margin + input.len() as u8, margin);
     print!("┗");
+    for _ in 0..margin {
+        print!("━")
+    }
     for x in input.chars() {
         print!("━");
+    }for _ in 0..margin {
+        print!("━")
     }
     println!("┛");
 }
@@ -41,8 +76,18 @@ fn main() {
         Err(_) => panic!("Name doesnt exist")
     }
 
-    println!("Welcome Back, {}", name);
-    println!("It has been {} seconds since the unix epoch", seconds_since_unix_epoch);
+    for args in env::args() {
+        if args == "-s" {
+            println!("It has been {} seconds since the unix epoch", seconds_since_unix_epoch);
+        }
+    }
+
+    let mut greeting: String = String::from("Welcome Back, ");
+    for x in String::from(name).chars() {
+        greeting.push(x);
+    }
+    boxed(greeting.as_str(), 2u8);
+
     println!("It has been {} minutes since the unix epoch", minutes_since_unix_epoch);
     println!("It has been {} hours since the unix epoch", hours_since_unix_epoch);
     println!("It has been {} days since the unix epoch", days_since_unix_epoch);
